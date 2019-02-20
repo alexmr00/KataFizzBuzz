2e2c3c441faedfa0903dd0e55f38f79627626f63
alexmr00 <langostademar777888@gmail.com>
2019-02-20 10:13:15 +0100

    1 devuelve 1

---
+++ src/Com/cifpcarballeira/katas/FizzBuzz/FizzBuzz.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Com.cifpcarballeira.katas.FizzBuzz;
+
+/**
+ *
+ * @author DAW115
+ */
+public class FizzBuzz {
+    public String getFizzBuzz(int i){
+        return "1";
+    }
+}
